c95ee8ddb89350ca87449112fb7384ea5332bdc8
{bigfive} <bigfive0907@gmail.com>
2024-02-08 16:19:46 +0900

    rustのホットリロードを追加

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,17 +1,14 @@
 use actix_web::{get, App, HttpServer, Responder};
 
-
 #[get("/")]
 async fn index() -> impl Responder {
-    "Hello world!"
+    "Hello world is Change in source Code if it is changed but today, he did not play with me. but today"
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(index)
-    })
-    .bind(("backend", 3000))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(index))
+        .bind(("backend", 3000))?
+        .run()
+        .await
 }
